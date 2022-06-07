00134eae00f726f0c6a311e0107a0025801e917d
Abdlhamidd <abdlhamid.hmd.20@gmail.com>
2022-06-07 21:36:50 +0700

    hamid

--- persegi.php
+++ persegi.php
@@ -1,4 +1,39 @@
 <?php
+// menyiapkan data untuk data persegi
+$data_persegi = [];
+
+// cek data di persegi.txt  yang ada di dalam direotori
+if (file_exists('./data/persegi.txt')) {
+    // jika ada maka baca file tersebut
+    $persegi = file('./data/persegi.txt');
+    // melakukan konversi dari format data serialize menjadi array
+    $data_persegi = unserialize($persegi[0]);
+}
+
+// FIA isset cek nilai dari form, issets cek apakah ada nilai dari form
+// jika di temukan todo melalui metode post maka akan di proses
+if (isset($_POST['sisi'])) {
+    // mengambil nilai dari form
+    $sisi = $_POST['sisi'];
+    $luas = $sisi * $sisi;
+    // menambahkan data ke dalam array
+    $data_persegi[] = [
+        'sisi' => $sisi,
+        'luas' => $luas
+    ];
+    // menyimpan data array ke dalam file
+    file_put_contents('./data/persegi.txt', serialize($data_persegi));
+}
+// menghapus data
+if (isset($_GET['hapus'])) {
+    // mengambil nilai dari form
+    $id = $_GET['hapus'];
+    // menghapus data
+    unset($data_persegi[$id]);
+    // menyimpan data array ke dalam file
+    file_put_contents('./data/persegi.txt', serialize($data_persegi));
+}
+
     include 'template/header.php';
     ?>
     <!-- Content -->
@@ -16,7 +51,7 @@
                                 <form action="persegi.php" method="post">
                                     <div class="form-group">
                                         <label for="sisi">Sisi</label>
-                                        <input type="number" class="form-control" name="sisi" id="sisi" placeholder="Masukkan sisi">
+                                        <input type="number" class="form-control" name="sisi" id="sisi" placeholder="Masukkan sisi" required>
                                     </div>
                                     <button type="submit" class="btn btn-primary">Hitung</button>
                                 </form>
@@ -38,6 +73,47 @@
                                         echo "Luas Persegi = $luas";
                                     }
                                 ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- list persegi -->
+        <div class="card p-3 bg-light mb-3">
+            <div class="card-body">
+                <div class="row mb-3">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header bg-dark text-light">
+                                List Persegi
+                            </div>
+                            <div class="card-body">
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Sisi</th>
+                                            <th>Luas</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                            // mengambil data dari array
+                                            foreach ($data_persegi as $key => $value) {
+                                                echo "<tr>";
+                                                echo "<td>".($key+1)."</td>";
+                                                echo "<td>".$value['sisi']."</td>";
+                                                echo "<td>".$value['luas']."</td>";
+                                                echo "<td>";
+                                                echo "<a href='?hapus=$key' class='btn btn-danger'>Hapus</a>";
+                                                echo "</td>";
+                                                echo "</tr>";
+                                            }
+                                        ?>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
--- segitiga.php
+++ segitiga.php
@@ -1,4 +1,40 @@
 <?php
+// menyiapkan data array untuk data segitiga
+$data_segitiga = [];
+
+// cek data di segitiga.txt  yang ada di dalam dereotori
+if (file_exists('./data/segitiga.txt')) {
+    // jika ada maka baca file tersebut
+    $segitiga = file('./data/segitiga.txt');
+    // melakukan konversi dari format data serialize menjadi array
+    $data_segitiga = unserialize($segitiga[0]);
+}
+
+// FIA isset cek nilai dari form, issets cek apakah ada nilai dari form
+// jika di temukan todo melalui metode post maka akan di proses
+if (isset($_POST['alas'])) {
+    // mengambil nilai dari form
+    $alas = $_POST['alas'];
+    $tinggi = $_POST['tinggi'];
+    $luas = $alas * $tinggi / 2;
+    // menambahkan data ke dalam array
+    $data_segitiga[] = [
+        'alas' => $alas,
+        'tinggi' => $tinggi,
+        'luas' => $luas
+    ];
+    // menyimpan data array ke dalam file
+    file_put_contents('./data/segitiga.txt', serialize($data_segitiga));
+}
+// menghapus data
+if (isset($_GET['hapus'])) {
+    // mengambil nilai dari form
+    $id = $_GET['hapus'];
+    // menghapus data
+    unset($data_segitiga[$id]);
+    // menyimpan data array ke dalam file
+    file_put_contents('./data/segitiga.txt', serialize($data_segitiga));
+}
     include 'template/header.php';
     ?>
     <!-- Content -->
@@ -43,6 +79,50 @@
                                         echo "Luas Segitiga = $luas";
                                     }
                                 ?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- daftar segitiga -->
+        <div class="card p-3 bg-light mb-3">
+            <div class="card-body">
+                <div class="row mb-3">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header bg-dark text-light">
+                                Daftar Segitiga
+                            </div>
+                            <div class="card-body">
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Alas</th>
+                                            <th>Tinggi</th>
+                                            <th>Luas</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php
+                                            $no = 1;
+                                            foreach($data_segitiga as $key => $value){
+                                                echo "<tr>";
+                                                echo "<td>$no</td>";
+                                                echo "<td>$value[alas]</td>";
+                                                echo "<td>$value[tinggi]</td>";
+                                                echo "<td>$value[luas]</td>";
+                                                echo "<td>
+                                                    <a href='segitiga.php?hapus=$key' class='btn btn-danger'>Hapus</a>
+                                                </td>";
+                                                echo "</tr>";
+                                                $no++;
+                                            }
+                                        ?>
+                                    </tbody>
+                                </table>
                             </div>
                         </div>
                     </div>
